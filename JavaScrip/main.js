3f5ab779382c20b303aacff794868dcf12b068d7
francisco <franciscosandoval2000@gmail.com>
2020-07-07 21:23:32 -0500

    componiendo errores javascript

--- JavaScrip/main.js
+++ JavaScrip/main.js
@@ -1,27 +1,34 @@
+var URLactual = document.title;
 $(function()
 {
 	'use strict';
-
-	/*carga*/
-	loadPage();
+	/*pantalla de carga*/
+ 	loadPage();
 	/*Mapa*/
+	var home= "Index";
+	var visitenos= "Visitenos";
 	
-	var map= L.map('mapa').setView([12.172672, -86.094478], 30);
+	if(URLactual === home || URLactual === visitenos){
+	   	
+		var map= L.map('mapa').setView([12.172672, -86.094478], 30);
 
 		
-	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-	}).addTo(map);
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(map);
 
-	L.marker([12.179762, 273.904181]).addTo(map)
-		.bindPopup('Servicios Electrónicos Profesionales.')
-		.openPopup();
+		L.marker([12.179762, 273.904181]).addTo(map)
+			.bindPopup('Servicios Electrónicos Profesionales.')
+			.openPopup();
+	}
+
 });
 
 
 $(function()
 {
 		'use strict';
+	
 	
 	var windoHeight = $('.header').height();
 	var barraAltura = $('.barra').innerHeight();
@@ -97,33 +104,26 @@ function icon()
 }
 
 function loadPage(){
-  
-	$.blockUI({
-	message:'<img src="Recursos/img/carga.gif" width="150" height="150"> <h3>Cargando ...</h3>', 
-	 css: { 
-        border: 'none', 
-		color:'white',
-        padding: '15px', 
-        backgroundColor: 'transparent;', 
-        '-webkit-border-radius': '0px', 
-        '-moz-border-radius': '0px', 
-         opacity: 0.5, 
-         color: '#fff' 
-        } }); 
+  	
+	var home = "index";
+	if(URLactual === home){
+	   	 	$.blockUI({
+			message:'<img src="Recursos/img/carga.gif" width="150" height="150"> <h3>Cargando ...</h3>', 
+			 css: { 
+				border: 'none', 
+				color:'white',
+				padding: '15px', 
+				backgroundColor: 'transparent;', 
+				'-webkit-border-radius': '0px', 
+				'-moz-border-radius': '0px', 
+				 opacity: 0.5, 
+				 color: '#fff' 
+				} }); 
+
+				setTimeout($.unblockUI, 3000);
+	   }
+
+}
+
  
-        setTimeout($.unblockUI, 3000);
-}
-
-
-
-function efectoBrinco()
-{
-	$(".animacion").click(function(){
-		$(".animacion").effect( "bounce", "slow" );
-	});
-	
-}
-
-
-
 
